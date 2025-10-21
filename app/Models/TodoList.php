0ba57e58ceb84115256fb4e17f37410268663912
DETA LUSIANA <detasandra19@gmail.com>
2025-10-21 16:33:58 +0700

    tambah file todo_list_deta_laravel

---
+++ app/Models/TodoList.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TodoList extends Model
+{
+    // tambahkan di sini
+    protected $fillable = [
+        'nama',
+        'tugas',
+        'deadline',
+        'status',
+    ];
+}
---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Daftar tugas</h1>
+    
+    <table Border="1" cellpadding="8">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Tugas</th>
+            <th>Deadline</th>
+            <th>Status</th>
+        </tr>
+        @foreach ($todo_lists as $item)
+            
+        <tr>
+            <td>{{ $item->id }}</td>
+            <td>{{ $item->nama }}</td>
+            <td>{{ $item->tugas }}</td>
+            <td>{{ $item->deadline }}</td>
+            <td>{{ $item->status }}</td>
+        </tr>
+            
+        @endforeach
+    </table>
+</body>
+</html>
