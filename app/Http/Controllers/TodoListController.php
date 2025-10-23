31c0cb101f683338af5b5242d68d70938db3f0c8
DETA LUSIANA <detasandra19@gmail.com>
2025-10-23 16:15:27 +0700

    update  file todo

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\TodoList;
+
 class TodoListController extends Controller
 {
     //
@@ -16,28 +17,53 @@ class TodoListController extends Controller
     }
     public function simpan(Request $request)
     {
-$request->validate([
-    'nama' => 'required',
-    'tugas' => 'required',
-    'deadline' => 'required|date',
-]);
-TodoList::create([
-    'nama' => $request->nama,
-    'tugas' => $request->tugas,
-    'deadline' => $request->deadline,
-    'status' => $request->status,
-]);
-return redirect('/todo');
+        $request->validate([
+            'nama' => 'required',
+            'tugas' => 'required',
+            'deadline' => 'required|date',
+        ]);
+        TodoList::create([
+            'nama' => $request->nama,
+            'tugas' => $request->tugas,
+            'deadline' => $request->deadline,
+            'status' => $request->status,
+        ]);
+        return redirect('/todo');
     }
-     public function tambah()
-     {
-return view('todo.form');
-     }
-      public function hapus($id)
-     {
-        $todo =TodoList::findOrFail($id);
+    public function tambah()
+    {
+        return view('todo.form');
+    }
+    public function hapus($id)
+    {
+        $todo = TodoList::findOrFail($id);
         $todo->delete();
 
-return redirect('/todo');
+        return redirect('/todo');
+    }
+    public function form_edit($id)
+     {
+        $todo = TodoList::findOrFail($id);
+        // dd($todo);
+        return view('todo.edit',[
+            'todo' =>$todo
+        ]);
+     }
+     public function update(Request $request,$id)
+     {
+        $request->validate([
+            'nama' => 'required',
+            'tugas' => 'required',
+            'deadline' => 'required|date',
+            'status' => 'required',
+        ]);
+        $todo = TodoList::findOrFail($id);
+        $todo->update([
+            'nama' => $request->nama,
+            'tugas' => $request->tugas,
+            'deadline' => $request->deadline,
+            'status' => $request->status,
+        ]);
+        return redirect('/todo');
      }
 }
